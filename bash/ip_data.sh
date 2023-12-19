bc86e29d0c615a5b6826686db24ff6176bd378e2
BMustafa97 <bilalmustafa1797@gmail.com>
2023-12-19 16:24:50 +0000

    update to ip scripty

--- bash/ip_data.sh
+++ bash/ip_data.sh
@@ -5,12 +5,10 @@ region=eu-west-2
 instance_count=$(aws ec2 describe-instances --region $region --filters "Name=tag:Name,Values=*$instance*" --query "Reservations[*].Instances[*].InstanceId" --output text | wc -l)
 echo "There are $instance_count instances with the name $instance"
 
-# for each instance gather the instance id and public ip address if it exists and the name tag
-for i in $(aws ec2 describe-instances --region $region --filters "Name=tag:Name,Values=*$instance*" --query "Reservations[*].Instances[*].InstanceId" --output text); do
-    echo "Instance ID: $i"
-    ip=$(aws ec2 describe-instances --region $region --instance-ids $i --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
+for instance in $(echo "$instances" | jq -r '.Reservations[].Instances[] | .InstanceId + "," + .PublicIpAddress + "," + (.Tags[] | select(.Key=="Name").Value)'); do
+    IFS=',' read -r id ip name <<< "$instance"
+    echo "Instance ID: $id"
     echo "Public IP: $ip"
-    name=$(aws ec2 describe-instances --region $region --instance-ids $i --query "Reservations[*].Instances[*].Tags[?Key=='Name'].Value" --output text)
     echo "Name: $name"
     echo "-------------------------"
 done
